ad8bcc812cc00d83af8180b2f5529ced43b1aec8
Bossaa55 <jordi.bossacoma2005@gmail.com>
2025-07-24 18:05:03 +0200

    fix: Bug fix to solve database connection not using env variable

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+
 engine = create_engine(
-    "mysql+pymysql://root:toor@mysql/voting_db",
+    f"mysql+pymysql://root:{DATABASE_PASSWORD}@mysql/voting_db",
     pool_size=30,
     max_overflow=5
 )
